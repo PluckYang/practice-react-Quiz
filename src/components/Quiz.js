8df40b988bae33f9e8e4ca2a3c09869adf6dce99
f-yang <pluck.yang@gmail.comgit config user.email pluck.yang@gmail.com>
2025-03-24 13:46:35 +0900

    Deriving Values, Outputting Questions & Registering Answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,33 @@
 import { useState } from "react";
+import { ListFormat } from "typescript";
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-  // control showing of current question, from dummy questions array
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   // to store user's choises of each question
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [[...prevUserAnswers, selectedAnswer]];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
